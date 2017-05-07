2d650da02a30c2ca670e7d52f109f9de4177a485
gray <lapenkov.sergey@gmail.com>
2017-05-07 23:37:13 +0300

    Add ServoCycleManager class and doing some code reformatting, add MPU6050 C functions sources files

---
+++ include/ServoCycleManager.h
@@ -0,0 +1,18 @@
+/*
+ * ServoCycleManager.h
+ *
+ *  Created on: 7 мая 2017 г.
+ *      Author: gray
+ */
+
+#ifndef SERVOCYCLEMANAGER_H_
+#define SERVOCYCLEMANAGER_H_
+
+class ServoCycleManager {
+public:
+	ServoCycleManager();
+
+	void powerUp();
+};
+
+#endif /* SERVOCYCLEMANAGER_H_ */
